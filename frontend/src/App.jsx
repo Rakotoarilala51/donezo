608ceb64555bb1f296426077702563318d472b9e
Rakotoarilala51 <maitreaina51@gmail.com>
2025-05-12 19:19:10 +0300

    adding main page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import AuthLayout from './layouts/AuthLayout'
 import Login from './components/auth/Login'
 import Singup from './components/auth/Singup'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { MainLayout } from './layouts/MainLayout'
+import Dashboard from './components/Dashboard/Dashboard'
 
 function App() {
 
@@ -13,6 +15,9 @@ function App() {
           <Route path='/auth' element={<AuthLayout/>}>
             <Route path='/auth/login' element={<Login/>}/>
             <Route path='/auth/signup' element={<Singup/>}/>
+          </Route>
+          <Route path="/" element={<MainLayout/>}>
+            <Route path='/dashboard' element={<Dashboard/>}/>
           </Route>
         </Routes>
       </Router>
---
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className='bg-white rounded-lg shadow pb-4 h-[200vh]'>Dashboard</div>
+  )
+}
+export default Dashboard
