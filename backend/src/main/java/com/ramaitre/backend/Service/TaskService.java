74fd742f33cbc69eeb43dda63fbcec56bce0b2c1
Rakotoarilala51 <maitreaina51@gmail.com>
2025-05-31 21:47:23 +0300

    creating task done

--- backend/src/main/java/com/ramaitre/backend/Service/TaskService.java
+++ backend/src/main/java/com/ramaitre/backend/Service/TaskService.java
@@ -2,6 +2,8 @@ package com.ramaitre.backend.Service;
 
 import com.ramaitre.backend.entity.Task;
 
+import java.util.List;
+
 public interface TaskService {
     public Task createTask(Task task);
 }
--- backend/src/main/java/com/ramaitre/backend/controller/TaskController.java
+++ backend/src/main/java/com/ramaitre/backend/controller/TaskController.java
@@ -1,23 +1,28 @@
 package com.ramaitre.backend.controller;
 
 import com.ramaitre.backend.Service.TaskService;
+import com.ramaitre.backend.entity.Project;
 import com.ramaitre.backend.entity.Task;
 import com.ramaitre.backend.entity.User;
+import com.ramaitre.backend.repository.ProjectRepository;
+import com.ramaitre.backend.repository.TaskRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("api/v1/task")
 public class TaskController {
-    private Task task;
     private TaskService taskService;
-
-    public ResponseEntity<Task> createTask(@RequestBody Task task1, @PathVariable Long projectId){
-        return null;
+    ProjectRepository projectRepository;
+    @PostMapping("{id}")
+    public ResponseEntity<Task> createTask(@RequestBody Task task1, @PathVariable Long id){
+        Project project = projectRepository.findById(id)
+                .orElseThrow(()->new RuntimeException("project not found"));
+        task1.setProject(project);
+        Task savedTask = taskService.createTask(task1);
+        return new ResponseEntity<>(savedTask, HttpStatus.CREATED);
     }
 }
