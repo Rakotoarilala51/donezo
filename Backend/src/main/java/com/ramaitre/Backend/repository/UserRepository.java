891f72d688e44f9c903f653cb41590cfef143218
Rakotoarilala51 <maitreaina51@gmail.com>
2025-07-02 06:52:31 +0300

    user repository

---
+++ Backend/src/main/java/com/ramaitre/Backend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ramaitre.Backend.repository;
+
+import com.ramaitre.Backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
